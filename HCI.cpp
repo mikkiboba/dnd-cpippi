db9db685e7d0783f1ff949ffa6f43c07deeb91b3
MillyMobby <97559615+MillyMobby@users.noreply.github.com>
2025-05-05 16:20:10 +0200

    UAAAAAA
    quack

---
+++ HCI.cpp
@@ -0,0 +1,13 @@
+﻿// HCI.cpp: definisce il punto di ingresso dell'applicazione.
+//
+#pragma once
+#include <opencv2/opencv.hpp>  
+#include "HCI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
